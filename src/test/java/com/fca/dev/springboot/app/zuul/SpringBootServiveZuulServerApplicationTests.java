bd0136beb4ac37e0352aafaf53ac1afcfaeb0b72
karunsane <karunsane@gmail.com>
2020-07-31 14:24:51 -0500

    init

---
+++ src/test/java/com/fca/dev/springboot/app/zuul/SpringBootServiveZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fca.dev.springboot.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiveZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
